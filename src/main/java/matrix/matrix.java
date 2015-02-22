5fa58abe789871712584a2717bf51301404284a0
Ai-ge-rim <slyamova.aigerim@gmail.com>
2015-02-22 23:08:45 +0400

    changes

--- src/main/java/matrix/matrix.java
+++ src/main/java/matrix/matrix.java
@@ -1,10 +1,18 @@
 package matrix;
 
+import javafx.scene.transform.MatrixType;
+import matrix1.Matrix;
+
 /**
  * Created by сулпак on 22.02.2015.
  */
 public class matrix {
     public static void main(String[] args) {
-        System.out.print("Hello");
-    }
+        Matrix matrix1 = new MatrixType ();
+        Matrix matrix2 = new MatrixType();
+            }
+
+
+
+
 }
---
+++ src/main/java/matrix1/Matrix.java
@@ -0,0 +1,21 @@
+package matrix1;
+
+/**
+ * Created by сулпак on 22.02.2015.
+ */
+public class Matrix {
+    public static void main(String[] args) {
+        int [][] matrix1 = new int[3][];
+        matrix1 = new int[3][3];
+        matrix1 [0] = new [] {0,1,6};
+        matrix1 [1] = new [] {2,7,1};
+        matrix1 [2] = new [] {9,0,2};
+
+        int [][] matrix2 = new int[3][];
+        matrix2  = new int [3][3];
+        matrix2 [0] = new [] {3,2,7};
+        matrix2 [1] = new [] {9,3,6};
+        matrix2 [2] = new [] {3,7,1};
+    }
+
+}
