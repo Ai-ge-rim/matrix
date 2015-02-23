5b47fbf8f068be30459db7f5773296b82c7d8e3b
Ai-ge-rim <slyamova.aigerim@gmail.com>
2015-02-23 09:51:01 +0400

    changes

--- src/main/java/matrix/matrix.java
+++ src/main/java/matrix/matrix.java
@@ -3,14 +3,22 @@ package matrix;
 import javafx.scene.transform.MatrixType;
 import matrix1.Matrix;
 
-/**
- * Created by сулпак on 22.02.2015.
- */
 public class matrix {
     public static void main(String[] args) {
-        Matrix matrix1 = new MatrixType ();
-        Matrix matrix2 = new MatrixType();
+        Matrix matrix1 = new Matrix ();
+        Matrix matrix2 = new Matrix();
+
+        Matrix matrixC = Matrix.multiply (matrix1, matrix2);
+        matrixC = matrix1.multiply(matrix2);
+        System.out.println(matrixC);
             }
+
+
+
+
+
+
+
 
 
 
--- src/main/java/matrix1/Matrix.java
+++ src/main/java/matrix1/Matrix.java
@@ -1,21 +1,29 @@
 package matrix1;
 
-/**
- * Created by сулпак on 22.02.2015.
- */
+import java.util.Arrays;
+
 public class Matrix {
     public static void main(String[] args) {
-        int [][] matrix1 = new int[3][];
-        matrix1 = new int[3][3];
-        matrix1 [0] = new [] {0,1,6};
-        matrix1 [1] = new [] {2,7,1};
-        matrix1 [2] = new [] {9,0,2};
-
-        int [][] matrix2 = new int[3][];
-        matrix2  = new int [3][3];
-        matrix2 [0] = new [] {3,2,7};
-        matrix2 [1] = new [] {9,3,6};
-        matrix2 [2] = new [] {3,7,1};
+        int [][] matrix1 = new int[3][3];
+        matrix1[0] = new int[]{0, 1, 6};
+        matrix1[1] = new int[]{2, 7, 1};
+        for (int i : matrix1[2] = new int[]{9, 0, 2}) {
+            System.out.println(Arrays.deepToString(matrix1));
+        }
+        int [][] matrix2 = new int[3][3];
+        matrix2[0] = new int[]{3, 2, 7};
+        matrix2[1] = new int[]{9, 3, 6};
+        for (int i : matrix2[2] = new int[]{3, 7, 1}) {
+            System.out.print(Arrays.deepToString(matrix2));
+        }
     }
 
+
+    public static Matrix multiply(Matrix matrix1, Matrix matrix2) {
+        return null;
+    }
+
+    public Matrix multiply(Matrix matrix2) {
+        return null;
+    }
 }
